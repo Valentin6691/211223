da98cf8efe9ccdac2ebab54ae2b8d7da641d65b5
Valentin <nitnelav6691@gmail.cov>
2023-12-17 20:49:47 +0300

    Задание 3

--- HomeWork211223/Program.cs
+++ HomeWork211223/Program.cs
@@ -64,8 +64,73 @@
 // программу, которая поменяет местами первую и 
 // последнюю строку массива.
 
-int[,] array = new int[5,5];
-int[,] array2 = new int[5,5];
+// int[,] array = new int[5,5];
+// int[,] array2 = new int[5,5];
+// void createArray()
+// {
+// for(int i = 0; i<array.GetLength(0); i++)
+//     {
+//     for(int j = 0; j<array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(1,10);
+//         }     
+//     }
+// }
+// void printArray()
+// {
+// for(int i = 0; i<array.GetLength(0); i++)
+//     {
+//     for(int j = 0; j<array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// void createArray2()
+// {
+// for(int i = 0; i<array2.GetLength(0); i++)
+//     {
+//     for(int j = 0; j<array2.GetLength(1); j++)
+//         {
+//            if(i>0  && i<array2.GetLength(0)-1)
+//            {
+//            array2[i,j] = array[i,j];
+//            }
+//            else
+//            {
+//             array2[i,j] = array[array.GetLength(0) - i - 1,j];
+//            }
+//         }     
+//     }
+// }
+// void printArray2()
+// {
+// for(int i = 0; i<array2.GetLength(0); i++)
+//     {
+//     for(int j = 0; j<array2.GetLength(1); j++)
+//         {
+//             Console.Write(array2[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// createArray();
+// printArray();
+// createArray2();
+// printArray2();
+
+// Задача 3: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с 
+// наименьшей суммой элементов.
+
+
+using System.ComponentModel;
+
+int[,] array = new int[3,3];
+
 void createArray()
 {
 for(int i = 0; i<array.GetLength(0); i++)
@@ -88,36 +153,27 @@ for(int i = 0; i<array.GetLength(0); i++)
     }
     Console.WriteLine();
 }
-void createArray2()
+Random rnd = new Random();
+void minSumRowArray()
 {
-for(int i = 0; i<array2.GetLength(0); i++)
+    int minSum = int.MaxValue;
+    int minSumRowIndex = -1;
+    for(int i = 0; i<array.GetLength(0); i++)
     {
-    for(int j = 0; j<array2.GetLength(1); j++)
+        int rowSum = 0;
+    for(int j = 0; j<array.GetLength(1); j++)
         {
-           if(i>0  && i<array2.GetLength(0)-1)
-           {
-           array2[i,j] = array[i,j];
-           }
-           else
-           {
-            array2[i,j] = array[array.GetLength(0) - i - 1,j];
-           }
-        }     
+            rowSum = rowSum + array[i,j];
+        } 
+            if(rowSum < minSum)
+                {
+                    minSum = rowSum;
+                    minSumRowIndex = i;
+                }  
     }
-}
-void printArray2()
-{
-for(int i = 0; i<array2.GetLength(0); i++)
-    {
-    for(int j = 0; j<array2.GetLength(1); j++)
-        {
-            Console.Write(array2[i,j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
+        Console.WriteLine(minSum); 
+        Console.WriteLine($"Строка с индексом {minSumRowIndex} с суммой: {minSum}");
 }
 createArray();
 printArray();
-createArray2();
-printArray2();
+minSumRowArray();
