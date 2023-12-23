1aebc1d176969cdba46810dd6008ad8e74a38838
Valentin <nitnelav6691@gmail.cov>
2023-12-23 11:36:04 +0300

    Задача 3

--- Homework281223/Program.cs
+++ Homework281223/Program.cs
@@ -31,18 +31,51 @@
 // Аккермана с помощью рекурсии. Даны два 
 // неотрицательных числа m и n. 
 
-int m = 3;
-int n = 8;
+// int m = 3;
+// int n = 8;
 
-int Ackermann(int m, int n)
+// int Ackermann(int m, int n)
+//     {
+//         if (m == 0)
+//             return n + 1;
+//         else if (n == 0)
+//             return Ackermann(m - 1, 1);
+//         else
+//             return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+
+// int result = Ackermann(m, n);
+// Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+
+// Задача 3: Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы.
+
+int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Элементы массива в прямом порядке: ");
+PrintArray(array, 0);
+
+void PrintArray(int[] array, int i)
     {
-        if (m == 0)
-            return n + 1;
-        else if (n == 0)
-            return Ackermann(m - 1, 1);
-        else
-            return Ackermann(m - 1, Ackermann(m, n - 1));
+        if (i == array.Length)
+        {
+            return;
+        }
+        Console.Write(array[i] + " ");
+        PrintArray(array, i + 1);
     }
+Console.WriteLine();
 
-int result = Ackermann(m, n);
-Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+Console.WriteLine("Элементы массива в обратном порядке: ");
+PrintArrayReverse(array, array.Length - 1);
+
+void PrintArrayReverse(int[] array, int i)
+    {
+        if (i < 0)
+        {
+            return;
+        }
+        Console.Write(array[i] + " ");
+        PrintArrayReverse(array, i - 1);
+    }
